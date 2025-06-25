aa46a4d72a76ff871ae17d0a296903b3504b1429
Irwin Jungreis <iljungr@csail.mit.edu>
2025-06-25 11:55:12 -0400

    Improved orbl.py -h

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 ORBL is a tool for measuring evolutionary conservation and constraint on the "ORFness"
 of an open reading frame (ORF) in a clade using multi-species whole genome alignments.
 It consists of two scores:
+
 - ORBLv measures conservation of the ORF by calculating the relative branch length of 
   the phylogenetic tree of species in an alignment that have an intact orthologous ORF, 
-  i.e., in which there is an aligned start codon, stop codon, and open reading frame. 
+  i.e., in which there is an aligned ATG start codon, stop codon, and open reading frame. 
   It is a number between 0 and 1, with larger numbers indicating more conservation.
-
 
 - ORBLq measures evolutionary constraint on the ORFness of a non-canonical ORF (ncORF)
   by calculating the quantile of its ORBLv score among the ORBLv scores of untranslated 
@@ -17,45 +17,42 @@ It consists of two scores:
   larger numbers indicating more constraint.
 
 ## Installation
-
 ```
 git clone https://github.com/iljungr/ORBL_tools.git
 ```
-
 Test by running:
 ```
 cd ORBL_tools
 python -m unittest test_ORBL
 ```
-
-ORBL has been tested in python 2.7.16 and python 3.12.4 but should work using other versions of
-python 2.7 and 3.
+ORBL has been tested in python 2.7.16 and python 3.12.4 but should work using other 
+versions of python 2.7 and 3.
 
 Requires an internet connection for downloading alignments.
 
 ## Usage Summary
-
 ```
-ORBL_tools/orbl.py (ALIGNMENT_SET [--orblq] [--components] [FILE_NAME] | -h,--help | -v,--version)
+python orbl.py (ALIGNMENT_SET [--orblq] [--components] [FILE] | -h,--help | -v,--version)
 
 Mandatory arguments:
-    ALIGNMENT_SET Specifies the multispecies alignment as defined by CodAlignView.
+   ALIGNMENT_SET Specifies the multispecies alignment as defined by CodAlignView. 
+                 Example: hg38_120mammals_primate.
+    
 
 Optional arguments:
-    -h, --help     Print this message and exit.
-    -v, --version  Print the orbl.py version number and exit.
-    --orblq        Report the ORBLq constraint score as well as the orblv conservation 
-                   score. This requires specifying the biotype-with-frameshift as the 
-                   third field on each input line. Currently, --orblq is only implemented 
-                   for certain alignment sets:
-    --components   Report the relative branch lengths of the species satisfying each
-                   or the three conditions used to consider the ORF to be conserved when
-                   calculating the ORBLv score, namely having an aligned start codon, 
-                   an aligned stop codon, and having an open reading frame (multiple of
-                   3 nucleotides and no in-frame stop codons).
-    FILE_NAME      Read input lines from FILE_NAME instead of standard input.
+   -h, --help     Print this message and exit.
+   -v, --version  Print the orbl.py version number and exit.
+   --orblq        Report the ORBLq constraint score as well as the orblv conservation 
+                  score. This requires specifying the biotype-with-frameshift as the 
+                  third field on each input line. Currently, --orblq is only 
+                  implemented for certain alignment sets.
+   --components   Report the relative branch lengths of the species satisfying each
+                  or the three conditions used to consider the ORF to be conserved when
+                  calculating the ORBLv score, namely having an aligned start codon, 
+                  an aligned stop codon, and having an open reading frame (multiple of
+                  3 nucleotides and no in-frame stop codons).
+   FILE           Read input lines from FILE instead of standard input.
 ```
-
 ## Details
 
 orbl.py takes input from the standard input or a specified file. Input consists of one 
@@ -96,14 +93,14 @@ is +1 or +2 depending on whether a ribosome reading the main frame would need to
 1 or 2 nucleotides to get in the frame of the ncORF. Valid values of
 biotype-with-frameshift are:
 ```
-   uORF, uoORF+1, uoORF+2, intORF+1, intORF+2, doORF+1, doORF+2, dORF, and lncRNA-ORF.
+uORF, uoORF+1, uoORF+2, intORF+1, intORF+2, doORF+1, doORF+2, dORF, and lncRNA-ORF.
 ```
 Biotype "mixed" is also allowed, but ORBLq is not implemented for this biotype and 
 results in a value of "NA".
 
 Currently, --orblq is only implemented for the following alignment sets:
 ```
-     hg38_120mammals_placental, hg38_120mammals_primate
+hg38_120mammals_placental, hg38_120mammals_primate
 ```
 Input lines may contain additional tab-separated fields, which are passed through to 
 the output and otherwise ignored. 
--- orbl.py
+++ orbl.py
@@ -13,7 +13,7 @@ Utility to calculate ORF Relative Branch Length (ORBL) scores for measuring ORFn
 conservation and constraint.
 """
 from __future__ import division, print_function
-import sys, os, itertools
+import sys, os, itertools, subprocess
 ThisDir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(ThisDir)) # ORBL_tools's parent
 from ORBL_tools.IntervalUtils import regionString_to_triples
@@ -30,8 +30,8 @@ MaxMaxCodons = 40000
 
 print_err = lambda *pArgs : print(*pArgs, file = sys.stderr)
 
-UsageStr = ('(ALIGNMENT_SET [--orblq] [--components] [FILE_NAME] '
-                '| -h,--help | -v,--version)')
+UsageStr = ('(ALIGNMENT_SET (e.g., hg38_120mammals_primate) [--orblq] [--components] '
+            '[FILE] |\n        -h,--help | -v,--version)')
 AlnsetsURL = 'https://data.broadinstitute.org/compbio1/cav.php?Alnsets'
 
 def main() :
@@ -39,7 +39,15 @@ def main() :
     if (check_arg('-h', remove = True, silent = True) or
         check_arg('--help', remove = True, silent = True)) :
         assert_num_args(0, UsageStr, exact = True)
-        print_err(UserGuide)
+        # Use "less" to display README.md, converting some of the markdown to terminal
+        #   escape codes.
+        with open(os.path.join(ThisDir, 'README.md'), 'rt') as readmeFile :
+            outStr = poor_mans_markdown_interpreter(readmeFile.read())
+            less = subprocess.Popen(['less', '-R'], stdin = subprocess.PIPE)
+            try :
+                less.communicate(input = outStr.encode('utf-8'))
+            except KeyboardInterrupt :
+                pass
         return
     if (check_arg('-v', remove = True, silent = True) or
         check_arg('--version', remove = True, silent = True)) :
@@ -170,112 +178,39 @@ def main() :
         if inFileName is not None :
             inFile.close()
 
-UserGuide = """
-Overview:
-
-ORBL is a tool for measuring evolutionary conservation and constraint on the "ORFness" 
-of an open reading frame (ORF) in a clade using multi-species whole genome alignments.
-It consists of two scores:
-
-- ORBLv measures conservation of the ORF by calculating the relative branch length of 
-  the phylogenetic tree of species in an alignment that have an intact orthologous ORF, 
-  i.e., in which there is an aligned start codon, stop codon, and open reading frame. 
-  It is a number between 0 and 1, with larger numbers indicating more conservation. 
-
-- ORBLq measures evolutionary constraint on the ORFness of a non-canonical ORF (ncORF)
-  by calculating the quantile of its ORBLv score among the ORBLv scores of untranslated 
-  ORFs of the same biotype and similar length. It too is a number between 0 and 1, with 
-  larger numbers indicating more constraint.
-
-Usage Summary: 
-
-ORBL_tools/orbl.py %s
-
-Mandatory arguments:
-    ALIGNMENT_SET Specifies the multispecies alignment as defined by CodAlignView.
-
-Optional arguments:
-    -h, --help     Print this message and exit.
-    -v, --version  Print the orbl.py version number and exit.
-    --orblq        Report the ORBLq constraint score as well as the orblv conservation 
-                   score. This requires specifying the biotype-with-frameshift as the 
-                   third field on each input line. Currently, --orblq is only implemented 
-                   for certain alignment sets:
-    --components   Report the relative branch lengths of the species satisfying each
-                   or the three conditions used to consider the ORF to be conserved when
-                   calculating the ORBLv score, namely having an aligned start codon, 
-                   an aligned stop codon, and having an open reading frame (multiple of
-                   3 nucleotides and no in-frame stop codons).
-    FILE_NAME      Read input lines from FILE_NAME instead of standard input.
-
-
-Details:
-
-orbl.py takes input from the standard input or a specified file. Input consists of one 
-or more lines, each representing an ORF in the reference species of a multispecies whole 
-genome alignment, specified by the ALIGNMENT_SET mandory argument. Alignment sets are
-defined by CodAlignView here: %s. 
-
-Each line contains two or more tab-separates fields. The first field is one or nore
-chromosomal intervals specifying the coordinates of an open reading frame in the 
-reference species of the alignment. The second field is the strand, either + or -. 
-The format of the first field consists of one or more intervals separated by plus signs: 
-   chrom:start1-end1+chrom:start2-end2+... 
-satisfying the requirement that:
-   start1 <= end1 < start2 <= end2... 
-(even if the region is on the minus strand). 
-All segments must be on the same chromosome.
-
-Example input line:
-chr10:1042727-1042762+chr10:1043301-1043321 +
-
-Coordinates follow the convention of GFF/GTF files and the UCSC genome browser, namely, 
-intervals go from the first base of the region to the last base, inclusive, and 
-coordinates are 1-based, i.e., the 1st base of the chromosome is position 1. Coordinates
-may include commas, which are ignored.
-
-If the region specified does not begin with ATG or end in a stop codon in the reference 
-species then both ORBLv and ORBLq will be "NA".
-
-If --orblq is specified, each input line must inlude a third field, containing the biotype
-and frameshift relative to the main ORF for biotypes uoORF, intORF, and doORF. Frameshift 
-is +1 or +2 depending on whether a ribosome reading the main frame would need to skip 
-1 or 2 nucleotides to get in the frame of the ncORF. Valid values of
-biotype-with-frameshift are:
-   uORF, uoORF+1, uoORF+2, intORF+1, intORF+2, doORF+1, doORF+2, dORF, and lncRNA-ORF.
-Biotype "mixed" is also allowed, but ORBLq is not implemented for this biotype and 
-results in a value of "NA".
-
-Currently, --orblq is only implemented for the following alignment sets:
-    %s
-
-Input lines may contain additional tab-separated fields, which are passed through to 
-the output and otherwise ignored. 
-
-Results are written to the standard output. There is one output line for each input line. 
-Each output line contains the input line followed by the orblv score, the orblq score 
-if the --orblq option is specified, and three additional fields if --components is 
-specified, namely the relative branch lengths of species having an aligned start codon, 
-an aligned stop codon, and an intact open reading frame.
-
-Credits:
-
-Questions should be directed to Irwin Jungreis at iljungr@csail.mit.edu.
-
-Citing ORBL:
-
-If you use ORBL, please cite, "High-quality peptide evidence for annotating 
-non-canonical open reading frames as human proteins" by Deutsch et al 
-(manuscript submitted). 
-
-More information about ORBL can be found in that paper. The specific requirements 
-for determining an ORF's biotype are reported in the supplementary materials.
-""" % (UsageStr, AlnsetsURL, ', '.join(get_supported_alnsets()))
-
 """
 Potential future arguments: 
     [--nTermExt] [--secondORF] [--allowAltStart] [--allowAllNearCognate]
 """
+
+def poor_mans_markdown_interpreter(inStr) :
+    """
+    Take a string in Markdown format and adjust it to something that can be printed on
+        a unix terminal.
+    Handles only an extremely limited subset of Markdown syntax:
+    - Converts headings (anything starting with '# ', '## ', etc) to bold text. For
+      level 1 heading, bold and underlined.
+    - Indents code blocks three spaces (but only recognizes them if
+      the starting and ending ``` are on lines with nothing else).
+    This is a pure python implementation. If that isn't required, use "rich" or
+        "markdown2" plus "html2text" for better Markdown interpretation.
+    """
+    escSeq = '\033['
+    inLines = inStr.split('\n')
+    outStr = ''
+    inCode = False
+    for line in inLines :
+        if line.strip() == '```' :
+            inCode = not inCode
+        elif inCode :
+            outStr += ' ' * 3 + line
+        elif line.startswith('#') and line.lstrip('#').startswith(' ') :
+            formatStr = '1;4m' if line.startswith('# ') else '1m'
+            outStr += escSeq + formatStr + line.lstrip('#')[1:] + escSeq + '0m'
+        else :
+            outStr += line
+        outStr += '\n'
+    return outStr
 
 if __name__ == '__main__' :
     main()
